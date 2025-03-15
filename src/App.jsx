b6a7551326720d8ee3559366aead806891f730f1
vrovsky <vrovsky@gmail.com>
2025-03-15 19:16:56 +0000

    pages devided

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header.jsx";
-import Intro from "./components/Intro.jsx";
-import Cases from "./components/Cases.jsx";
-import Courses from "./components/Courses.jsx";
-import Reviews from "./components/Reviews.jsx";
-import Consultation from "./components/Consultation.jsx";
-import AboutMe from "./components/AboutMe.jsx";
-import Footer from "./components/Footer.jsx";
+
+import HomePage from "./pages/HomePage.jsx";
 import ConsultationPage from "./pages/ConsultationPage.jsx";
 
-function HomePage() {
-  return (
-    <>
-      <Header />
-      <Intro />
-      <Cases />
-      <Courses />
-      <Reviews />
-      <Consultation />
-      <AboutMe />
-      <Footer />
-    </>
-  );
-}
 function App() {
   return (
     <Router>
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Header from "../components/Header.jsx";
+import Intro from "../components/Intro.jsx";
+import Cases from "../components/Cases.jsx";
+import Courses from "../components/Courses.jsx";
+import Reviews from "../components/Reviews.jsx";
+import Consultation from "../components/Consultation.jsx";
+import AboutMe from "../components/AboutMe.jsx";
+import Footer from "../components/Footer.jsx";
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <Intro />
+      <Cases />
+      <Courses />
+      <Reviews />
+      <Consultation />
+      <AboutMe />
+      <Footer />
+    </>
+  );
+}
